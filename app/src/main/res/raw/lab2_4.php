6304e23b0806c57dcc21e9b72b1553fb75285015
khang <khangndph20612@fpt.edu.vn>
2023-07-10 09:08:11 +0700

    ok

--- app/src/main/res/raw/lab2_4.php
+++ app/src/main/res/raw/lab2_4.php
@@ -3,24 +3,24 @@
  $b=$_POST['b'];
  $c=$_POST['c'];
  $delta=pow($b, 2)-4 * $a * $c;
- $rs=""
+ $rs="";
  if($a==0){
  	if (b==0) {
- 		$rs="Vo so nghiem"
+ 		$rs="Vo so nghiem";
   	}else{
-  		$x1=-$c/$b
-  		$rs="PT co nghiem duy nhat: " .$x1
+  		$x1=-$c/$b;
+  		$rs="PT co nghiem duy nhat: " .$x1;
     }
  }else{
  	if ($delta<0) {
-       $rs="PT vo nghiem" 
+       $rs="PT vo nghiem" ;
 	}else{
 	if ($delta==0) {
 		$x1=((-$b+sqrt($delta))/(2 * $a));
 		$x2=((-$b-sqrt($delta))/(2 * $a));
-		$rs="PT co hai nghiem phan biet : x1= ".$x1."; x2= ".$x2
+		$rs="PT co hai nghiem phan biet : x1= ".$x1."; x2= ".$x2;
 	}
  }
 }
-echo "Ket qua" .$rs;
+echo "Ket qua " .$rs;
 ?>
